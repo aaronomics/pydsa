b8627821f7a290b144ac2e41d2a504941843810e
aaronomics <aaronwweiss00@gmail.com>
2024-10-23 14:53:18 -0500

    more big o notes the return

--- bigO.py
+++ bigO.py
@@ -5,12 +5,9 @@ def print_items(n):
 
 print_items(10)
 
-"""
-O(n) is always linear
 
+# O(n) is always linear
 
-
-"""
 
 
 def print_items2(n):
@@ -21,10 +18,31 @@ def print_items2(n):
 
 print_items2(10)
 
-"""
-nested for loops are O(n^2)
 
-you can drop non-dominant elements
-- if you have nested for loops and a single loop in the same function
-- the nested loop will dominate over time
-"""
+# nested for loops are O(n^2)
+
+# you can drop non-dominant elements
+# - if you have nested for loops and a single loop in the same function
+# - the nested loop will dominate over time
+
+# O(log_n) - binary search, works on sorted data
+
+
+# if a function has 2 different parameters...
+
+def print_items3(a,b):
+    for i in range(a):
+        print(i)
+    for j in range(b):
+        print(j)
+
+# this would be O(a + b), not O(n), but technically it is still O(n)
+
+# and this....
+def print_items4(a,b):
+    for i in range(a):
+        for j in range(b):
+            print(i, j)
+
+# would be O(a*b)
+
